aabb8fced3b7aecfee22682c3bfd3cd19bb1b571
lianran9113 <15104239113@163.com>
2019-04-25 20:52:39 +0800

    计时器加注释

--- src/ES5JS/list.js
+++ src/ES5JS/list.js
@@ -1,1 +1,15 @@
-console.log(1)
+
+    var nowdate = new Date();
+	
+    var fuldate = new Date(nowdate.getFullYear(),nowdate.getMonth(),nowdate.getDate()+365);
+    
+    setInterval(function(){
+    var nowDate = new Date();
+    var cdate = fuldate - nowDate;
+    var day = parseInt(cdate/86400000);
+    var hours = parseInt(cdate%86400000/3600000);
+    var minutes = parseInt(cdate%86400000%3600000/60000);
+    var seconds = parseInt(cdate%86400000%3600000%60000/1000);
+      $(".shengyu span").html("剩余：" + day + "天"+ hours + "小时" + minutes + "分钟" + seconds + "秒");
+})
+
